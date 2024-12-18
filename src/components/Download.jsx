532321a3748f6d9126bd66eb120de795d95180b2
KQ <foongquan03@gmail.com>
2024-12-18 12:15:54 +0800

    commit

--- src/components/Download.jsx
+++ src/components/Download.jsx
@@ -18,10 +18,10 @@ export default function DownloadVideo() {
         try {
             const response = await fetch(`${import.meta.env.VITE_API_BASE_URL}/v/download`, {
                 method: "POST",
+                credentials: 'include',
                 headers: {
                     "Content-Type": "application/json",
                 },
-                // credentials: 'include',
                 body: JSON.stringify({ 
                     url: youtubeLink,
                     playlist_id: playlistId,
